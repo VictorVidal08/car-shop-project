2fdd041b23dad5bd86f2b6a59fe506f6082d73ac
VictorVidal08 <victorvidal08@hotmail.com>
2022-10-25 16:11:54 -0300

    feat: creating IMotorcycle interface

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+import VehicleZodSchema from './IVehicle';
+
+const MotorcycleZodSchema = VehicleZodSchema.extend({
+  category: z.enum(['Trail', 'Custom', 'Street']),
+  engineCapacity: z.number({
+    required_error: 'EngineCapacity is required',
+    invalid_type_error: 'EngineCapacity must be a number',
+  }).positive().lte(2500),
+});
+
+export type IMotorcycle = z.infer<typeof MotorcycleZodSchema>;
+
+export { MotorcycleZodSchema };
